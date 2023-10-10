7c8966d3c950ae3ba8b542e5f35c7041bf1ce6d9
Veritoa45 <molinariveronica478@gmail.com>
2023-10-10 08:53:13 -0300

    version 5 - con las opiniones y el log in

---
+++ js/popup.js
@@ -0,0 +1,449 @@
+const btnAbrirModal1 = 
+document.querySelector(".btn-abrir-modal1");
+const btnCerrarModal1 =
+document.querySelector(".btn-cerrar-modal1");
+const modal1 =
+document.querySelector(".modal1");
+
+btnAbrirModal1.addEventListener("click",()=> {
+    modal1.showModal();
+})
+
+btnCerrarModal1.addEventListener("click",()=> {
+    modal1.close();
+})
+
+const btnAbrirModal2 = 
+document.querySelector(".btn-abrir-modal2");
+const btnCerrarModal2 =
+document.querySelector(".btn-cerrar-modal2");
+const modal2 =
+document.querySelector(".modal2");
+
+btnAbrirModal2.addEventListener("click",()=> {
+    modal2.showModal();
+})
+
+btnCerrarModal2.addEventListener("click",()=> {
+    modal2.close();
+})
+
+const btnAbrirModal3 = 
+document.querySelector(".btn-abrir-modal3");
+const btnCerrarModal3 =
+document.querySelector(".btn-cerrar-modal3");
+const modal3 =
+document.querySelector(".modal3");
+
+btnAbrirModal3.addEventListener("click",()=> {
+    modal3.showModal();
+})
+
+btnCerrarModal3.addEventListener("click",()=> {
+    modal3.close();
+})
+
+const btnAbrirModal4 = 
+document.querySelector(".btn-abrir-modal4");
+const btnCerrarModal4 =
+document.querySelector(".btn-cerrar-modal4");
+const modal4 =
+document.querySelector(".modal4");
+
+btnAbrirModal4.addEventListener("click",()=> {
+    modal4.showModal();
+})
+
+btnCerrarModal4.addEventListener("click",()=> {
+    modal4.close();
+})
+
+const btnAbrirModal5 = 
+document.querySelector(".btn-abrir-modal5");
+const btnCerrarModal5 =
+document.querySelector(".btn-cerrar-modal5");
+const modal5 =
+document.querySelector(".modal5");
+
+btnAbrirModal5.addEventListener("click",()=> {
+    modal5.showModal();
+})
+
+btnCerrarModal5.addEventListener("click",()=> {
+    modal5.close();
+})
+
+const btnAbrirModal6 = 
+document.querySelector(".btn-abrir-modal6");
+const btnCerrarModal6 =
+document.querySelector(".btn-cerrar-modal6");
+const modal6 =
+document.querySelector(".modal6");
+
+btnAbrirModal6.addEventListener("click",()=> {
+    modal6.showModal();
+})
+
+btnCerrarModal6.addEventListener("click",()=> {
+    modal6.close();
+})
+
+const btnAbrirModal7 = 
+document.querySelector(".btn-abrir-modal7");
+const btnCerrarModal7 =
+document.querySelector(".btn-cerrar-modal7");
+const modal7 =
+document.querySelector(".modal7");
+
+btnAbrirModal7.addEventListener("click",()=> {
+    modal7.showModal();
+})
+
+btnCerrarModal7.addEventListener("click",()=> {
+    modal7.close();
+})
+
+const btnAbrirModal8 = 
+document.querySelector(".btn-abrir-modal8");
+const btnCerrarModal8 =
+document.querySelector(".btn-cerrar-modal8");
+const modal8 =
+document.querySelector(".modal8");
+
+btnAbrirModal8.addEventListener("click",()=> {
+    modal8.showModal();
+})
+
+btnCerrarModal8.addEventListener("click",()=> {
+    modal8.close();
+})
+
+const btnAbrirModal9 = 
+document.querySelector(".btn-abrir-modal9");
+const btnCerrarModal9 =
+document.querySelector(".btn-cerrar-modal9");
+const modal9 =
+document.querySelector(".modal9");
+
+btnAbrirModal9.addEventListener("click",()=> {
+    modal9.showModal();
+})
+
+btnCerrarModal9.addEventListener("click",()=> {
+    modal9.close();
+})
+
+const btnAbrirModal10 = 
+document.querySelector(".btn-abrir-modal10");
+const btnCerrarModal10 =
+document.querySelector(".btn-cerrar-modal10");
+const modal10 =
+document.querySelector(".modal10");
+
+btnAbrirModal10.addEventListener("click",()=> {
+    modal10.showModal();
+})
+
+btnCerrarModal10.addEventListener("click",()=> {
+    modal10.close();
+})
+
+const btnAbrirModal11 = 
+document.querySelector(".btn-abrir-modal11");
+const btnCerrarModal11 =
+document.querySelector(".btn-cerrar-modal11");
+const modal11 =
+document.querySelector(".modal11");
+
+btnAbrirModal11.addEventListener("click",()=> {
+    modal11.showModal();
+})
+
+btnCerrarModal11.addEventListener("click",()=> {
+    modal11.close();
+})
+
+const btnAbrirModal12 = 
+document.querySelector(".btn-abrir-modal12");
+const btnCerrarModal12 =
+document.querySelector(".btn-cerrar-modal12");
+const modal12 =
+document.querySelector(".modal12");
+
+btnAbrirModal12.addEventListener("click",()=> {
+    modal12.showModal();
+})
+
+btnCerrarModal12.addEventListener("click",()=> {
+    modal12.close();
+})
+
+const btnAbrirModal13 = 
+document.querySelector(".btn-abrir-modal13");
+const btnCerrarModal13 =
+document.querySelector(".btn-cerrar-modal13");
+const modal13 =
+document.querySelector(".modal13");
+
+btnAbrirModal13.addEventListener("click",()=> {
+    modal13.showModal();
+})
+
+btnCerrarModal13.addEventListener("click",()=> {
+    modal13.close();
+})
+
+const btnAbrirModal14 = 
+document.querySelector(".btn-abrir-modal14");
+const btnCerrarModal14 =
+document.querySelector(".btn-cerrar-modal14");
+const modal14 =
+document.querySelector(".modal14");
+
+btnAbrirModal14.addEventListener("click",()=> {
+    modal14.showModal();
+})
+
+btnCerrarModal14.addEventListener("click",()=> {
+    modal14.close();
+})
+
+const btnAbrirModal15 = 
+document.querySelector(".btn-abrir-modal15");
+const btnCerrarModal15 =
+document.querySelector(".btn-cerrar-modal15");
+const modal15 =
+document.querySelector(".modal15");
+
+btnAbrirModal15.addEventListener("click",()=> {
+    modal15.showModal();
+})
+
+btnCerrarModal15.addEventListener("click",()=> {
+    modal15.close();
+})
+
+const btnAbrirModal16 = 
+document.querySelector(".btn-abrir-modal16");
+const btnCerrarModal16 =
+document.querySelector(".btn-cerrar-modal16");
+const modal16 =
+document.querySelector(".modal16");
+
+btnAbrirModal16.addEventListener("click",()=> {
+    modal16.showModal();
+})
+
+btnCerrarModal16.addEventListener("click",()=> {
+    modal16.close();
+})
+
+const btnAbrirModal17 = 
+document.querySelector(".btn-abrir-modal17");
+const btnCerrarModal17 =
+document.querySelector(".btn-cerrar-modal17");
+const modal17 =
+document.querySelector(".modal17");
+
+btnAbrirModal17.addEventListener("click",()=> {
+    modal17.showModal();
+})
+
+btnCerrarModal1.addEventListener("click",()=> {
+    modal17.close();
+})
+
+const btnAbrirModal18 = 
+document.querySelector(".btn-abrir-modal18");
+const btnCerrarModal18 =
+document.querySelector(".btn-cerrar-modal18");
+const modal18 =
+document.querySelector(".modal18");
+
+btnAbrirModal18.addEventListener("click",()=> {
+    modal18.showModal();
+})
+
+btnCerrarModal18.addEventListener("click",()=> {
+    modal18.close();
+})
+
+const btnAbrirModal19 = 
+document.querySelector(".btn-abrir-modal19");
+const btnCerrarModal19 =
+document.querySelector(".btn-cerrar-modal19");
+const modal19 =
+document.querySelector(".modal19");
+
+btnAbrirModal19.addEventListener("click",()=> {
+    modal19.showModal();
+})
+
+btnCerrarModal19.addEventListener("click",()=> {
+    modal19.close();
+})
+
+const btnAbrirModal20 = 
+document.querySelector(".btn-abrir-modal20");
+const btnCerrarModal20 =
+document.querySelector(".btn-cerrar-modal20");
+const modal20 =
+document.querySelector(".modal20");
+
+btnAbrirModal20.addEventListener("click",()=> {
+    modal20.showModal();
+})
+
+btnCerrarModal20.addEventListener("click",()=> {
+    modal20.close();
+})
+
+const btnAbrirModal21 = 
+document.querySelector(".btn-abrir-modal21");
+const btnCerrarModal21 =
+document.querySelector(".btn-cerrar-modal21");
+const modal21 =
+document.querySelector(".modal21");
+
+btnAbrirModal21.addEventListener("click",()=> {
+    modal21.showModal();
+})
+
+btnCerrarModal21.addEventListener("click",()=> {
+    modal21.close();
+})
+
+const btnAbrirModal22 = 
+document.querySelector(".btn-abrir-modal22");
+const btnCerrarModal22 =
+document.querySelector(".btn-cerrar-modal22");
+const modal22 =
+document.querySelector(".modal22");
+
+btnAbrirModal22.addEventListener("click",()=> {
+    modal22.showModal();
+})
+
+btnCerrarModal22.addEventListener("click",()=> {
+    modal22.close();
+})
+
+const btnAbrirModal23 = 
+document.querySelector(".btn-abrir-modal23");
+const btnCerrarModal23 =
+document.querySelector(".btn-cerrar-modal23");
+const modal23 =
+document.querySelector(".modal23");
+
+btnAbrirModal23.addEventListener("click",()=> {
+    modal23.showModal();
+})
+
+btnCerrarModal23.addEventListener("click",()=> {
+    modal23.close();
+})
+
+const btnAbrirModal24 = 
+document.querySelector(".btn-abrir-modal24");
+const btnCerrarModal24 =
+document.querySelector(".btn-cerrar-modal24");
+const modal24 =
+document.querySelector(".modal24");
+
+btnAbrirModal24.addEventListener("click",()=> {
+    modal24.showModal();
+})
+
+btnCerrarModal24.addEventListener("click",()=> {
+    modal24.close();
+})
+
+const btnAbrirModal25 = 
+document.querySelector(".btn-abrir-modal25");
+const btnCerrarModal25 =
+document.querySelector(".btn-cerrar-modal25");
+const modal25 =
+document.querySelector(".modal25");
+
+btnAbrirModal25.addEventListener("click",()=> {
+    modal25.showModal();
+})
+
+btnCerrarModal25.addEventListener("click",()=> {
+    modal25.close();
+})
+
+const btnAbrirModal26 = 
+document.querySelector(".btn-abrir-modal26");
+const btnCerrarModal26 =
+document.querySelector(".btn-cerrar-modal26");
+const modal26 =
+document.querySelector(".modal26");
+
+btnAbrirModal26.addEventListener("click",()=> {
+    modal26.showModal();
+})
+
+btnCerrarModal26.addEventListener("click",()=> {
+    modal26.close();
+})
+
+const btnAbrirModal27 = 
+document.querySelector(".btn-abrir-modal27");
+const btnCerrarModal27 =
+document.querySelector(".btn-cerrar-modal27");
+const modal27 =
+document.querySelector(".modal27");
+
+btnAbrirModal27.addEventListener("click",()=> {
+    modal27.showModal();
+})
+
+btnCerrarModal27.addEventListener("click",()=> {
+    modal27.close();
+})
+
+const btnAbrirModal28 = 
+document.querySelector(".btn-abrir-modal28");
+const btnCerrarModal28 =
+document.querySelector(".btn-cerrar-modal28");
+const modal28 =
+document.querySelector(".modal28");
+
+btnAbrirModal28.addEventListener("click",()=> {
+    modal1.showModal();
+})
+
+btnCerrarModal28.addEventListener("click",()=> {
+    modal1.close();
+})
+
+const btnAbrirModal29 = 
+document.querySelector(".btn-abrir-modal29");
+const btnCerrarModal29 =
+document.querySelector(".btn-cerrar-modal29");
+const modal29 =
+document.querySelector(".modal29");
+
+btnAbrirModal29.addEventListener("click",()=> {
+    modal29.showModal();
+})
+
+btnCerrarModal29.addEventListener("click",()=> {
+    modal29.close();
+})
+
+const btnAbrirModal30 = 
+document.querySelector(".btn-abrir-modal30");
+const btnCerrarModal30 =
+document.querySelector(".btn-cerrar-modal30");
+const modal30 =
+document.querySelector(".modal30");
+
+btnAbrirModal30.addEventListener("click",()=> {
+    modal30.showModal();
+})
+
+btnCerrarModal30.addEventListener("click",()=> {
+    modal30.close();
+})
